ea01f5a1f4f0a0847855666787b2b6622f2455aa
KB-Toast <kevin.brusson@gmail.com>
2024-01-31 01:50:29 +0100

    i should go to sleep

--- routes/carts.js
+++ routes/carts.js
@@ -7,16 +7,7 @@ const Cart = require('../models/carts');
 
 // CREATE
 router.post('/new', (req, res) => {
-  let userId;
-  if (!req.body.userId || req.body.userId === undefined) {
-    userId = '65b8f89debf5988b2efd772b';
-    // defaults to "bob lebrave" userId
-  } else {
-    userId = req.body.userId;
-  }
-
   const newCart = new Cart({
-    userId,
     tripId: req.body.tripId,
     paid: false,
   });
@@ -25,31 +16,16 @@ router.post('/new', (req, res) => {
 });
 
 // READ
-// get all items in cart for userId
-router.post('/getCart', (req, res) => {
-  let userId;
-  if (!req.body.userId || req.body.userId === undefined) {
-    userId = '65b8f89debf5988b2efd772b';
-    // defaults to "bob lebrave" userId
-  } else {
-    userId = req.body.userId;
-  }
-
-  Cart.find({ userId, paid: false }).then((userCarts) =>
+// get all items in cart
+router.get('/getCart', (req, res) => {
+  Cart.find({ paid: false }).then((userCarts) =>
     res.json({ result: true, userCarts })
   );
 });
 
 // get all items in booking for userId
-router.post('/getBooking', (req, res) => {
-  let userId;
-  if (!req.body.userId || req.body.userId === undefined) {
-    userId = '65b8f89debf5988b2efd772b';
-    // defaults to "bob lebrave" userId
-  } else {
-    userId = req.body.userId;
-  }
-  Cart.find({ userId, paid: true }).then((userBookings) =>
+router.get('/getBooking', (req, res) => {
+  Cart.find({ paid: true }).then((userBookings) =>
     res.json({ result: true, userBookings })
   );
 });
@@ -59,20 +35,12 @@ router.post('/getBooking', (req, res) => {
 router.put('/update', (req, res) => {
   const tripId = req.body.tripId;
 
-  let userId;
-  if (!req.body.userId || req.body.userId === undefined) {
-    userId = '65b8f89debf5988b2efd772b';
-    // defaults to "bob lebrave" userId
-  } else {
-    userId = req.body.userId;
-  }
-
-  Cart.findOne({ userId, tripId }).then((cartFound) => {
+  Cart.findOne({ tripId }).then((cartFound) => {
     if (!cartFound) {
       return res.json({ result: false, error: 'Trip not found' });
     } else {
       console.log(cartFound);
-      Cart.updateOne({ userId, tripId }, { paid: !cartFound.paid }).then(
+      Cart.updateOne({ tripId }, { paid: !cartFound.paid }).then(
         (userUpdated) => {
           return res.json({ result: true, userUpdated });
         }
@@ -85,19 +53,12 @@ router.put('/update', (req, res) => {
 // remove trip from cart and/or booking
 router.delete('/delete', (req, res) => {
   const tripId = req.body.tripId;
-  let userId;
-  if (!req.body.userId || req.body.userId === undefined) {
-    userId = '65b8f89debf5988b2efd772b';
-    // defaults to "bob lebrave" userId
-  } else {
-    userId = req.body.userId;
-  }
 
-  Cart.findOne({ userId, tripId }).then((cartFound) => {
+  Cart.findOne({ tripId }).then((cartFound) => {
     if (!cartFound) {
       return res.json({ result: false, error: 'cart not found' });
     } else {
-      Cart.deleteOne({ userId, tripId }).then((cartDeleted) => {
+      Cart.deleteOne({ tripId }).then((cartDeleted) => {
         return res.json({ result: true, cartDeleted });
       });
     }
